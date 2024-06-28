5c7ef6accf928ab6fb8974e0564d1104672509e5
tob5ter <146123566+tob5ter@users.noreply.github.com>
2024-06-28 22:58:30 +0000

    I will be back

--- src/_05_for_loops/_2_badgers/badgers.java
+++ src/_05_for_loops/_2_badgers/badgers.java
@@ -3,16 +3,26 @@ package _05_for_loops._2_badgers;
 public class badgers {
     public static void main(String[] args) {
         
-
+for(int z=0;z<2;z+=1){
     for(int x=0;x<12;x+=1){
         System.out.print("Badger");
         if (x<11) {
             System.out.print(", ");
     
         }
+        
 
     }
+    System.out.println();
+    for(int n=0;n<2;n+=1){
+        System.out.print("Mushroom");
+        if (n<1) {
+            System.out.print(", ");
+        }
+    }
+    System.out.println();
 
-
+}
+System.out.print("A Snake!!!");
 }
 }
